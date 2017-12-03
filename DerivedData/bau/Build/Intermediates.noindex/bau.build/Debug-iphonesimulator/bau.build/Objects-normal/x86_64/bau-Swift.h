581a24f6043d2eb0de0ffca392e30320bc7f8fc0
Daniel Pape <danielwpape@gmail.com>
2017-12-03 21:00:59 +0000

    Added menu

--- DerivedData/bau/Build/Intermediates.noindex/bau.build/Debug-iphonesimulator/bau.build/Objects-normal/x86_64/bau-Swift.h
+++ DerivedData/bau/Build/Intermediates.noindex/bau.build/Debug-iphonesimulator/bau.build/Objects-normal/x86_64/bau-Swift.h
@@ -175,6 +175,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 @import UIKit;
 @import SpriteKit;
 @import CoreGraphics;
+@import Foundation;
 #endif
 
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
@@ -251,6 +252,22 @@ SWIFT_CLASS("_TtC3bau18GameViewController")
 - (void)updateLabels;
 - (void)success;
 - (IBAction)tapResetButton:(id _Nonnull)sender;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+@end
+
+@class UITableView;
+@class UITableViewCell;
+
+SWIFT_CLASS("_TtC3bau20LevelsViewController")
+@interface LevelsViewController : UITableViewController
+@property (nonatomic, copy) NSArray<NSString *> * _Nonnull levels;
+- (void)viewDidLoad;
+- (void)didReceiveMemoryWarning;
+- (NSInteger)numberOfSectionsInTableView:(UITableView * _Nonnull)tableView SWIFT_WARN_UNUSED_RESULT;
+- (NSInteger)tableView:(UITableView * _Nonnull)tableView numberOfRowsInSection:(NSInteger)section SWIFT_WARN_UNUSED_RESULT;
+- (UITableViewCell * _Nonnull)tableView:(UITableView * _Nonnull)tableView cellForRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath SWIFT_WARN_UNUSED_RESULT;
+- (nonnull instancetype)initWithStyle:(UITableViewStyle)style OBJC_DESIGNATED_INITIALIZER;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
 @end
